0aa218eeee57d8f3e2cfeb39215a070a5cfccd49
linjiajian999 <linjiajian@MacBook-Pro.local>
2017-12-11 21:39:25 +0800

    update

--- src/copy-file.ts
+++ src/copy-file.ts
@@ -14,16 +14,32 @@ export default async function removeToTarget() {
 
   const target = path.resolve(basePath, config.target[selfEnv])
   const src = path.resolve(basePath, config.source)
+
+  let lastSrc: string = ''
+  let lastTime: number = Number.MIN_SAFE_INTEGER
+
+  const srcFiles: string[] = fs.readdirSync(src)
+  srcFiles.forEach(srcfile => {
+    const srcfiledir = src + '/' + srcfile
+    const stats: fs.Stats = fs.statSync(srcfiledir)
+    stats.birthtime
+    if (stats.birthtime.getTime() > lastTime) {
+      lastSrc = srcfiledir
+      lastTime = stats.birthtime.getTime()
+    }
+    lastSrc = src + '/' + srcfile
+  })
   console.log('开始删除')
   remove(target).then(() => {
     console.log('删除完成')
     console.log('开始复制')
-    copy(src, target)
+    copy(lastSrc, target)
   })
   .then(() => {
     console.log('复制完成')
   })
   .catch(err => {
+    console.log('error')
     console.log(err)
   })
 }
